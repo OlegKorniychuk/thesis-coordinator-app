23ff55c8c9c38bd4771934ea006953a4430eb37f
OlegKorniychuk <kornoleg576@gmail.com>
2025-05-09 16:33:54 +0300

    feat: styled admin navbar

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'tc-admin',
   templateUrl: './admin.component.html',
-  imports: [RouterOutlet, RouterLink],
+  styleUrl: './admin.component.scss',
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
 })
 export class AdminComponent {}
